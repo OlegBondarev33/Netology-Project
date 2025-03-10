5d96759f15e3c3c9442693b1e9bd07aa71302db1
OlegBondarev33 <bondarev3396@gmail.com>
2025-03-11 00:06:15 +0300

    Update security_groups.tf

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,1 +1,32 @@
-TEXT
+resource "yandex_vpc_security_group" "bastion_sg" {
+  name        = "bastion-sg"
+  description = "Security group for bastion host"
+  network_id  = yandex_vpc_network.default_network.id
+
+  ingress {
+    protocol       = "tcp"
+    port           = 22
+    v4_cidr_blocks = [var.your_ip_address] # Only allow SSH from your IP
+  }
+
+  egress {
+    protocol       = "tcp"
+    port           = 22
+    v4_cidr_blocks = ["0.0.0.0/0"]  # Allow SSH to anywhere (can be restricted further)
+    #from_port      = 1
+    #to_port        = 65535
+  }
+}
+
+resource "yandex_vpc_security_group" "internal_vms_sg" {
+  name        = "internal-vms-sg"
+  description = "Security group for internal VMs"
+  network_id  = yandex_vpc_network.default_network.id
+
+  ingress {
+    protocol       = "tcp"
+    port           = 22
+    v4_cidr_blocks = ["${yandex_compute_instance.bastion.network_interface[0].ip_address}/32"]
+    description = "Allow SSH from Bastion Host"
+  }
+}
