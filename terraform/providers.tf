da7e3adf49186fec30ad7e778a5329617af64a12
OlegBondarev33 <bondarev3396@gmail.com>
2025-03-11 00:06:01 +0300

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,1 +1,15 @@
-TEXT
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "~> 0.139.0"  # Specify the desired version
+    }
+  }
+}
+
+provider "yandex" {
+  token     = var.yc_token
+  cloud_id  = var.yc_cloud_id
+  folder_id = var.yc_folder_id
+  zone      = var.default_zone # You can also specify the zone here or in each resource
+}
