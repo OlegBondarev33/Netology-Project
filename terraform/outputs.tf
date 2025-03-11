d0ddfdc6f463d44e74afd4b53dbfc334b53f0ec2
OlegBondarev33 <bondarev3396@gmail.com>
2025-03-11 22:30:06 +0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,23 @@
 output "vm_a_internal_ip" {
   value       = yandex_compute_instance.vm_a.network_interface[0].ip_address
-  description = "Internal IP address of VM A"
+  description = "IP address internal-vm-a"
 }
 
 output "vm_b_internal_ip" {
   value       = yandex_compute_instance.vm_b.network_interface[0].ip_address
-  description = "Internal IP address of VM B"
+  description = "IP address internal-vm-b"
 }
 
 output "bastion_public_ip" {
   value       = yandex_compute_instance.bastion.network_interface[0].nat_ip_address
-  description = "Public IP address of the Bastion host"
+  description = "IP address Bastion"
 }
 
-output "alb_public_ip" {
-  value = yandex_alb_load_balancer.load_balancer.listener[0].endpoint[0].address[0].external_ipv4_address[0].address
-  description = "Public IP address of the Application Load Balancer"
+output "bastion_internal_ip" {
+  value       = yandex_compute_instance.bastion.network_interface[0].ip_address
+  description = "IP address Bastion"
+}
+
+output "target_group_id" {
+  value = yandex_alb_target_group.target_group.id
 }
