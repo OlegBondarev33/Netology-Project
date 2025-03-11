f8586f0d9428dbca1f031bab4449706067abef46
OlegBondarev33 <bondarev3396@gmail.com>
2025-03-11 23:11:16 +0300

    Create kibana.tf

---
+++ terraform/kibana.tf
@@ -0,0 +1,26 @@
+resource "yandex_compute_instance" "kibana_vm" {
+  name        = "kibana-vm"
+  zone        = var.default_zone
+  platform_id = "standard-v1"
+
+  resources {
+    cores  = 2
+    memory = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.default_subnet_a.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "vboxuser:${var.ssh_public_key}"
+  }
+}
