ca2d9a750549f9ca3e95faf4d37c116501988a29
OlegBondarev33 <bondarev3396@gmail.com>
2025-03-16 23:58:30 +0300

    Create httpRouter.tf

---
+++ terraform/httpRouter.tf
@@ -0,0 +1,22 @@
+resource "yandex_alb_http_router" "http-router" {
+  name = "http-router"
+}
+
+resource "yandex_alb_virtual_host" "root" {
+  name           = "root-virtual-host"
+  http_router_id = yandex_alb_http_router.http_router.id
+  route {
+    name = "root-path"
+    http_route {
+      http_match {
+        path {
+          prefix = "/"
+        }
+      }
+      http_route_action {
+        backend_group_id = yandex_alb_backend_group.web_alb_bg.id
+        timeout          = "3s"
+      }
+    }
+  }
+}
