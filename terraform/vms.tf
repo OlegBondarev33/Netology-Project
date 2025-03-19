625c9b99f6c23ad29f4ba141e3617cb2f6f62e22
OlegBondarev33 <bondarev3396@gmail.com>
2025-03-19 23:10:33 +0300

    Update vms.tf

--- terraform/vms.tf
+++ terraform/vms.tf
@@ -53,3 +53,84 @@ resource "yandex_compute_instance" "vm_b" {
   }
   depends_on = [yandex_vpc_subnet.default_subnet_b] 
 }
+
+resource "yandex_compute_instance" "kibana_vm" {
+  name        = "kibana-vm"
+  zone        = var.default_zone
+  platform_id = "standard-v1"
+
+  resources {
+    cores  = 2
+    memory = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.default_subnet_a.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "vboxuser:${var.ssh_public_key}"
+  }
+}
+
+resource "yandex_compute_instance" "elasticsearch_vm" {
+  name        = "elasticsearch-vm"
+  zone        = var.default_zone
+  platform_id = "standard-v1"
+
+  resources {
+    cores  = 2
+    memory = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.default_subnet_a.id
+    nat       = false
+  }
+
+  metadata = {
+    ssh-keys = "vboxuser:${var.ssh_public_key}"
+  }
+}
+
+resource "yandex_compute_instance" "bastion" {
+  name        = "bastion-host"
+  zone        = var.default_zone
+  platform_id = "standard-v1"
+
+  resources {
+    cores  = 2
+    memory = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.default_subnet_a.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "vboxuser:${var.ssh_public_key}"
+  }
+}
