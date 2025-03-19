413265fa68b7766238fb43bb347c8d29a45c94e7
OlegBondarev33 <bondarev3396@gmail.com>
2025-03-20 00:15:55 +0300

    Update disk.tf

--- terraform/disk.tf
+++ terraform/disk.tf
@@ -3,6 +3,37 @@ resource "yandex_compute_disk" "disk_vm_a" {
   type     = "network-hdd"
   zone     = "ru-central1-a"
   image_id = "fd8arv291825p8bbvmb1"
-  size     = 10
-  
+  size     = 10  
+}
+
+resource "yandex_compute_disk" "disk_vm_b" {
+  name     = "epde2vn4sg2dntvtbvm1"
+  type     = "network-hdd"
+  zone     = "ru-central1-b"
+  image_id = "fd8arv291825p8bbvmb1"
+  size     = 10  
+}
+
+resource "yandex_compute_disk" "disk_vm_a" {
+  name     = "fhm6neu5hmimvvtgrts2"
+  type     = "network-hdd"
+  zone     = "ru-central1-a"
+  image_id = "fd8arv291825p8bbvmb1"
+  size     = 10  
+}
+
+resource "yandex_compute_disk" "disk_vm_a" {
+  name     = "fhm6neu5hmimvvtgrts2"
+  type     = "network-hdd"
+  zone     = "ru-central1-a"
+  image_id = "fd8arv291825p8bbvmb1"
+  size     = 10  
+}
+
+resource "yandex_compute_disk" "disk_vm_a" {
+  name     = "fhm6neu5hmimvvtgrts2"
+  type     = "network-hdd"
+  zone     = "ru-central1-a"
+  image_id = "fd8arv291825p8bbvmb1"
+  size     = 10  
 }
