43e3ba6fed1135a8cf9d3130da3f8ae689c14209
OlegBondarev33 <bondarev3396@gmail.com>
2025-03-20 01:26:07 +0300

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -33,3 +33,26 @@ resource "yandex_alb_backend_group" "backend_group" {
     }
   }
 }
+
+resource "yandex_alb_http_router" "http-router" {
+  name = "http-router"
+}
+
+resource "yandex_alb_virtual_host" "virt-host" {
+  name           = "virt-host"
+  http_router_id = yandex_alb_http_router.http_router.id
+  route {
+    name = "root-path"
+    http_route {
+      http_match {
+        path {
+          prefix = "/"
+        }
+      }
+      http_route_action {
+        backend_group_id = yandex_alb_backend_group.web_alb_bg.id
+        timeout          = "3s"
+      }
+    }
+  }
+}
