7d125a2dcbb25c8f976c4082eae7144fc4e37f0b
OlegBondarev33 <bondarev3396@gmail.com>
2025-03-20 01:02:09 +0300

    Update security_groups.tf

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,23 +1,3 @@
-resource "yandex_vpc_security_group" "bastion_sg" {
-  name        = "bastion-sg"
-  description = "Security group for bastion host"
-  network_id  = yandex_vpc_network.default_network.id
-
-  ingress {
-    protocol       = "tcp"
-    port           = 22
-    v4_cidr_blocks = [var.your_ip_address]
-  }
-
-  egress {
-    protocol       = "tcp"
-    port           = 22
-    v4_cidr_blocks = ["0.0.0.0/0"]  
-    #from_port      = 1
-    #to_port        = 65535
-  }
-}
-
 resource "yandex_vpc_security_group" "internal_vms_sg" {
   name        = "internal-vms-sg"
   description = "Security group for internal VMs"
@@ -35,22 +15,37 @@ resource "yandex_vpc_security_group" "internal_vms_sg" {
     v4_cidr_blocks = ["0.0.0.0/0"]  
     description = "Allow HTTP traffic"
   }
+  ingress {  
+    protocol       = "tcp"
+    port           = 10050
+    v4_cidr_blocks = ["0.0.0.0/0"]  
+    description = "For Zabbix"
+  }
+    egress {
+    protocol       = "ANY"
+    v4_cidr_blocks = ["0.0.0.0/0"]  
+  }
 }
--------------------------------------------------------------
 
-resource "yandex_vpc_security_group" "internal-vms-sg" {
-  name       = "internal-vms-sg"
-  network_id = yandex_vpc_network.default_network.id
-  description = "Security group for internal VMs"
+
+resource "yandex_vpc_security_group" "bastion_sg" {
+  name        = "bastion-sg"
+  description = "Security group for bastion host"
+  network_id  = yandex_vpc_network.default_network.id
 
   ingress {
+    protocol       = "tcp"
+    port           = 22
+    v4_cidr_blocks = [var.your_ip_address]
+  }
+  ingress {
     protocol       = "ANY"
-    v4_cidr_blocks = ["${yandex_compute_instance.bastion.network_interface[0].ip_address}/32"]
+    port           = 10050
+    v4_cidr_blocks = [var.your_ip_address]
   }
 
   egress {
-    protocol       = "ANY"
-    description    = "allow any outgoing connections"
-    v4_cidr_blocks = ["0.0.0.0/0"]
+    protocol       = "tcp"
+    v4_cidr_blocks = ["0.0.0.0/0"]  
   }
 }
