cedbc24572ca6f91456dbc2deb9ac35471c8bd3b
OlegBondarev33 <bondarev3396@gmail.com>
2025-03-22 10:29:03 +0300

    Update security_groups.tf

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,123 +1,166 @@
+resource "yandex_vpc_security_group" "bastion_sg" {
+  name        = "bastion-sg"
+  description = "Bastion"
+  network_id  = yandex_vpc_network.default_network.id
+
+  ingress {
+    protocol       = "tcp"
+    port           = 22
+    v4_cidr_blocks = [var.my-address]
+    description = "SSH my Internet"
+  }
+
+  ingress {
+    protocol       = "ANY"
+    port           = 10050
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    description = "For Zabbix"
+  }
+
+  egress {
+    protocol       = "ANY"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    #from_port      = 1
+    #to_port        = 65535
+  }
+}
+
 resource "yandex_vpc_security_group" "internal_vms_sg" {
   name        = "internal-vms-sg"
-  description = "Security group for internal VMs"
+  description = "VM"
   network_id  = yandex_vpc_network.default_network.id
 
   ingress {
     protocol       = "tcp"
     port           = 22
     v4_cidr_blocks = ["${yandex_compute_instance.bastion.network_interface[0].ip_address}/32"]
-    description = "Allow SSH from Bastion Host"
+    description = "SSH from Bastion"
   }
-  ingress {  
+
+  ingress {
     protocol       = "tcp"
     port           = 80
-    v4_cidr_blocks = ["0.0.0.0/0"]  
-    description = "Allow HTTP traffic"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    description = "TCP port nginx"
   }
-  ingress {  
-    protocol       = "tcp"
+
+ ingress {
+    protocol       = "ANY"
     port           = 10050
-    v4_cidr_blocks = ["0.0.0.0/0"]  
+    v4_cidr_blocks = ["0.0.0.0/0"]
     description = "For Zabbix"
   }
-    egress {
-    protocol       = "ANY"
-    v4_cidr_blocks = ["0.0.0.0/0"]  
-  }
+
 }
 
-
-resource "yandex_vpc_security_group" "bastion_sg" {
-  name        = "bastion-sg"
-  description = "Security group for bastion host"
+resource "yandex_vpc_security_group" "elastic" {
+  name        = "elastic-sg"
+  description = "ElasticSearch"
   network_id  = yandex_vpc_network.default_network.id
 
   ingress {
     protocol       = "tcp"
     port           = 22
-    v4_cidr_blocks = [var.your_ip_address]
+    v4_cidr_blocks = [var.my-address]
+    description = "SSH for Bastion"
   }
+
   ingress {
     protocol       = "ANY"
-    port           = 10050
-    v4_cidr_blocks = [var.your_ip_address]
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    description = "Internet"
   }
 
   egress {
-    protocol       = "tcp"
-    v4_cidr_blocks = ["0.0.0.0/0"]  
+    protocol       = "ANY"
+    v4_cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-
-resource "yandex_vpc_security_group" "alb-w2q" {
-  name        = "alb-w2q"
-  description = "Security group for Balancer"
+resource "yandex_vpc_security_group" "balanser" {
+  name        = "balanser"
+  description = "Balanser"
   network_id  = yandex_vpc_network.default_network.id
 
-  ingress {
-    protocol       = "tcp"
-    port           = 30080
-    v4_cidr_blocks = ["158.160.177.58/32"]
-  }
   ingress {
     protocol       = "ANY"
     port           = 80
     v4_cidr_blocks = ["0.0.0.0/0"]
+    description = "Internet"
+  }
+
+  ingress {
+      description       = "Status Balanser"
+      port              = 30080
+      predefined_target = "loadbalancer_healthchecks"
+      protocol          = "TCP"
   }
 
   egress {
-    protocol       = "tcp"
-    v4_cidr_blocks = ["0.0.0.0/0"]  
+    protocol       = "ANY"
+    v4_cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-
-resource "yandex_vpc_security_group" "elastic-sg" {
-  name        = "elastic-sg"
-  description = "Security group for ElasticSearch"
+resource "yandex_vpc_security_group" "kibana" {
+  name        = "kibana"
+  description = "Kibana"
   network_id  = yandex_vpc_network.default_network.id
 
-  ingress {
-    protocol       = "tcp"
-    port           = 22
-    v4_cidr_blocks = [var.your_ip_address]
-  }
   ingress {
     protocol       = "ANY"
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    protocol       = "TCP"
+    port           = 22
+    v4_cidr_blocks = [var.my-address]
+  }
+
+ingress {
+    protocol       = "TCP"
+    port           = 443
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ingress {
+    protocol       = "TCP"
+    port           = 9200
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
-    protocol       = "tcp"
-    v4_cidr_blocks = ["0.0.0.0/0"]  
+    protocol       = "ANY"
+    v4_cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
 
 resource "yandex_vpc_security_group" "zabbix" {
-  name        = "zabbix"
-  description = "Security group for Zabbix"
+  name        = "zabbix-vm"
+  description = "Zabbix"
   network_id  = yandex_vpc_network.default_network.id
 
-  ingress {
-    protocol       = "tcp"
-    port           = 22
-    v4_cidr_blocks = [var.your_ip_address]
-  }
-  ingress {
-    protocol       = "ANY"
-    port           = 80
-    v4_cidr_blocks = ["0.0.0.0/0"]
-  }
   ingress {
     protocol       = "ANY"
     port           = 10050
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    protocol       = "TCP"
+    port           = 22
+    v4_cidr_blocks = [var.my-address]
+  }
+
+ingress {
+    protocol       = "TCP"
+    port           = 80
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     protocol       = "ANY"
-    v4_cidr_blocks = ["0.0.0.0/0"]  
+    v4_cidr_blocks = ["0.0.0.0/0"]
   }
 }
